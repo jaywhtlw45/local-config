5cdfc39698c4c20ba19f60081ffc3d5870e48a42
Jason Whitlow <jason.whitlow45@gmail.com>
2025-10-21 19:19:59 -0700

    add

--- bashrc/DESKTOP-MMKQQS8.sh
+++ bashrc/DESKTOP-MMKQQS8.sh
@@ -1,13 +1,14 @@
+alias vim="nvim"
+
 # Start SSH agent if not running
-
-glazewm_src="$REPO_CONFIG_PATH/.glzr"
-glazewm_dest="$HOME/"
-
 [[ "$BASHRC_VERBOSE" ]] && echo "Starting ssh-agent"
 if [ -z "$SSH_AUTH_SOCK" ] || ! ssh-add -l &>/dev/null; then
   eval "$(ssh-agent -s)" >/dev/null
   ssh-add /c/users/jay/.ssh/github 2>/dev/null
 fi
+
+glazewm_src="$REPO_CONFIG_PATH/.glzr"
+glazewm_dest="$HOME/"
 
 [[ "$BASHRC_VERBOSE" ]] && echo "Copying $glazewm_src to $glazewm_dest"
 if [[ -d "$glazewm_src" ]]; then
