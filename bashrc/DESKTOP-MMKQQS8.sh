a6c674b5751a3c71faaf6d34bf92e5d5ae843b0d
Jason Whitlow <jason.whitlow45@gmail.com>
2025-10-21 20:03:29 -0700

    add laptop .glzr toggle

--- bashrc/DESKTOP-MMKQQS8.sh
+++ bashrc/DESKTOP-MMKQQS8.sh
@@ -7,10 +7,16 @@ if [ -z "$SSH_AUTH_SOCK" ] || ! ssh-add -l &>/dev/null; then
   ssh-add /c/users/jay/.ssh/github 2>/dev/null
 fi
 
-glazewm_src="$REPO_CONFIG_PATH/.glzr"
 glazewm_dest="$HOME/"
+
+if [[ "$MACHINE_TYPE" == "laptop" ]]; then
+  glazewm_src="$REPO_CONFIG_PATH/glazewm_alternatives/laptop/.glzr"
+else
+  glazewm_src="$REPO_CONFIG_PATH/.glzr"
 
 [[ "$BASHRC_VERBOSE" ]] && echo "Copying $glazewm_src to $glazewm_dest"
 if [[ -d "$glazewm_src" ]]; then
   cp -r "$glazewm_src" "$glazewm_dest"
 fi
+
+
